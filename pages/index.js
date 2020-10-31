8ac74c7f347c0bffcb01c5457421d248f0f7c8f1
badiushkni <zadnipru_iu@yahoo.com>
2020-10-31 15:00:42 +0200

    Add Todo Submit Form

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { table, minifyRecords } from "./api/utils/Airtable";
 import { TodosContext } from "../context/TodosContext";
 import { useEffect, useContext } from "react";
 import auth0 from "./api/utils/auth0";
+import TodoForm from "../components/TodoForm";
 
 export default function Home({ initialTodos, user }) {
   const { todos, setTodos } = useContext(TodosContext);
@@ -21,10 +22,15 @@ export default function Home({ initialTodos, user }) {
       <Navbar user={user} />
 
       <main>
-        <h1>Todo App</h1>
-        <ul>
-          {todos && todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
-        </ul>
+        {user && (
+          <>
+            <h1 className="text-2xl text-center mb-4">My Todos</h1>
+            <TodoForm />
+            <ul>
+              {todos && todos.map((todo) => <Todo key={todo.id} todo={todo} />)}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   );
