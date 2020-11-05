ca9c8f463213d15a65c64651b5de17409a0fbcb5
badiushkni <zadnipru_iu@yahoo.com>
2020-11-05 09:54:23 +0200

    Add entries ownership control

--- pages/api/deleteTodo.js
+++ pages/api/deleteTodo.js
@@ -1,7 +1,10 @@
 import { table, getMinifiedRecord } from "./utils/Airtable";
+import auth0 from "./utils/auth0";
+import OwnsRecord from "./middleware/OwnsRecord";
 
-export default async (req, res) => {
+export default OwnsRecord(async (req, res) => {
   const { id } = req.body;
+  const { user } = await auth0.getSession(req);
 
   try {
     const deletedRecords = await table.destroy([id]);
@@ -12,4 +15,4 @@ export default async (req, res) => {
     res.statusCode = 500;
     res.json({ msg: "Something went wrong" });
   }
-};
+});
--- pages/api/updateTodo.js
+++ pages/api/updateTodo.js
@@ -1,7 +1,10 @@
 import { table, getMinifiedRecord } from "./utils/Airtable";
+import auth0 from "./utils/auth0";
+import OwnsRecord from "./middleware/OwnsRecord";
 
-export default async (req, res) => {
+export default OwnsRecord(async (req, res) => {
   const { id, fields } = req.body;
+  const { user } = await auth0.getSession(req);
 
   try {
     const updatedRecords = await table.update([{ id, fields }]);
@@ -12,4 +15,4 @@ export default async (req, res) => {
     res.statusCode = 500;
     res.json({ msg: "Something went wrong" });
   }
-};
+});
